31a59d0b9285c9597c241969b241f1c6247e6b2e
shani nemni <shanemni1@gmail.com>
2020-08-10 22:27:31 +0300

    products data

--- src/server/database/ProductsList.js
+++ src/server/database/ProductsList.js
@@ -2,19 +2,20 @@ const colors = require('../common/Colors');
 const sizes = require('../common/Sizes');
 
 module.exports = [
-{id:1,name:'user1',categoryID:1,colors:[colors.BLACK,colors.GREEN,colors.RED],sizes:[sizes.S,sizes.XS,sizes.M,sizes.L],brand:"Adika",price:30,currency:"$"},
-{id:1,name:'user1',categoryID:2,colors:[colors.PINK,colors.GREEN,colors.RED],sizes:[sizes.M,sizes.L],brand:"HM",price:20,currency:"$"},
-{id:2,name:'user2',categoryID:3,colors:[colors.RED],sizes:[sizes.S,sizes.XS,sizes.L],brand:"ZARA",price:100,currency:"$"},
-{id:3,name:'user3',categoryID:1,colors:[colors.PINK,colors.BLACK],sizes:[sizes.S,sizes.XS,sizes.L],brand:"ZARA",price:100,currency:"$"},
-{id:4,name:'user4',categoryID:2,colors:[colors.RED,colors.WHITE],sizes:[sizes.S,sizes.XL,sizes.L,sizes.XXL],brand:"HM",price:35,currency:"$"},
-{id:5,name:'user5',categoryID:3,colors:[colors.PINK,colors.BLACK,colors.BEGIE,colors.YELLOW],sizes:[sizes.XL,sizes.L],brand:"Adika",price:25,currency:"$"},
-{id:6,name:'user6',categoryID:1,colors:[colors.ORANGE,colors.BLACK,colors.PINK,colors.BLUE],sizes:[sizes.XL,sizes.L,sizes.S,sizes.M],brand:"Adika",price:60,currency:"$"},
-{id:7,name:'user7',categoryID:2,colors:[colors.CYAN,colors.PINK_CLAY,colors.SILVER,colors.BLACK],sizes:[sizes.XL,sizes.L,sizes.S],brand:"Gong",price:30,currency:"$"},
-{id:8,name:'user8',categoryID:3,colors:[colors.SILVER,colors.WHITE_CREAM,colors.BEGIE,colors.BROWN],sizes:[],brand:"Swiss",price:100,currency:"$"},
-{id:9,name:'user9',categoryID:1,colors:[colors.BROWN,colors.CYAN],sizes:[sizes.XL,sizes.L],brand:"ZARA",price:40,currency:"$"},
-{id:10,name:'user10',categoryID:2,colors:[colors.BLUE,colors.PINK,colors.BROWN],sizes:[sizes.M,sizes.L],brand:"Adika",price:200,currency:"$"},
-{id:11,name:'user11',categoryID:3,colors:[colors.SILVER,colors.BEGIE,colors.WHITE_CREAM],sizes:[],brand:"Swiss",price:120,currency:"$"},
-{id:12,name:'user12',categoryID:1,colors:[colors.GREEN,colors.BROWN],sizes:[sizes.S,sizes.XXS,sizes.M,sizes.L,sizes.XXL],brand:"Castro",price:80,currency:"$"},
-{id:13,name:'user13',categoryID:2,colors:[colors.PINK_CLAY,colors.PINK,colors.BEGIE],sizes:[sizes.XS,sizes.S,sizes.XXS,sizes.M,sizes.L],brand:"Castro",price:75,currency:"$"},
-{id:14,name:'user14',categoryID:3,colors:[colors.BROWN,colors.CYAN],sizes:[sizes.M,sizes.L],brand:"Castro",price:69,currency:"$"}
+{id:1,name:'Flower Dress',categoryID:1,colors:[colors.BLACK,colors.GREEN,colors.RED],sizes:[sizes.S,sizes.XS,sizes.M,sizes.L],brand:"Adika",price:30,currency:"$",countSeller:30,creationDate:'10/08/2019'},
+{id:1,name:'Skinny Jeans',categoryID:2,colors:[colors.PINK,colors.GREEN,colors.RED],sizes:[sizes.M,sizes.L],brand:"HM",price:20,currency:"$",countSeller:20,creationDate:'03/08/2020'},
+{id:2,name:'Flower Tshirt',categoryID:3,colors:[colors.RED],sizes:[sizes.S,sizes.XS,sizes.L],brand:"ZARA",price:100,currency:"$",countSeller:0,creationDate:'10/08/2020'},
+{id:3,name:'Mini Dress',categoryID:1,colors:[colors.PINK,colors.BLACK],sizes:[sizes.S,sizes.XS,sizes.L],brand:"ZARA",price:100,currency:"$",countSeller:1,creationDate:'12/08/2020'},
+{id:4,name:'High Waist Jeans',categoryID:2,colors:[colors.RED,colors.WHITE],sizes:[sizes.S,sizes.XL,sizes.L,sizes.XXL],brand:"HM",price:35,currency:"$",countSeller:23,creationDate:'10/07/2019'},
+{id:5,name:'Nike Tshirt',categoryID:3,colors:[colors.PINK,colors.BLACK,colors.BEGIE,colors.YELLOW],sizes:[sizes.XL,sizes.L],brand:"Adika",price:25,currency:"$",countSeller:100,creationDate:'12/11/2019'},
+{id:6,name:'MIDI Dress',categoryID:1,colors:[colors.ORANGE,colors.BLACK,colors.PINK,colors.BLUE],sizes:[sizes.XL,sizes.L,sizes.S,sizes.M],brand:"Adika",price:60,currency:"$",countSeller:13,creationDate:'11/03/2020'},
+{id:7,name:'Nike pants',categoryID:2,colors:[colors.CYAN,colors.PINK_CLAY,colors.SILVER,colors.BLACK],sizes:[sizes.XL,sizes.L,sizes.S],brand:"Gong",price:30,currency:"$",countSeller:18,creationDate:'10/05/2019'},
+{id:8,name:'Adidas Tshirt',categoryID:3,colors:[colors.SILVER,colors.WHITE_CREAM,colors.BEGIE,colors.BROWN],sizes:[sizes.XL,sizes.L],brand:"Gong",price:100,currency:"$",countSeller:19,creationDate:'12/05/2019'},
+{id:9,name:'Oversized Denim Jacket',categoryID:4,colors:[colors.BROWN,colors.CYAN],sizes:[sizes.XL,sizes.L],brand:"ZARA",price:40,currency:"$",countSeller:10,creationDate:'10/05/2020'},
+{id:10,name:'Bag',categoryID:5,colors:[colors.BLUE,colors.PINK,colors.BROWN],sizes:[],brand:"Adika",price:200,currency:"$",countSeller:0,creationDate:'03/07/2020'},
+{id:11,name:'Flower Belt',categoryID:5,colors:[colors.SILVER,colors.BEGIE,colors.WHITE_CREAM],sizes:[],brand:"Swiss",price:120,currency:"$",countSeller:2,creationDate:'10/06/2020'},
+{id:12,name:'Flower Hat',categoryID:5,colors:[colors.GREEN,colors.BROWN],sizes:[sizes.S,sizes.XXS,sizes.M,sizes.L,sizes.XXL],brand:"Castro",price:80,currency:"$",countSeller:20,creationDate:'20/06/2020'},
+{id:13,name:'Sunglasses',categoryID:5,colors:[colors.PINK_CLAY,colors.PINK,colors.BEGIE],sizes:[],brand:"Castro",price:75,currency:"$",countSeller:1,creationDate:'10/08/2020'},
+{id:14,name:'Watch',categoryID:5,colors:[colors.PINK_CLAY,colors.PINK,colors.BEGIE],sizes:[],brand:"Castro",price:75,currency:"$",countSeller:122,creationDate:'29/03/2020'},
+{id:14,name:'Nike Shoes',categoryID:6,colors:[colors.BROWN,colors.CYAN],sizes:[sizes.UK_2,sizes.UK_3,sizes.UK_4],brand:"Castro",price:69,currency:"$",countSeller:10,creationDate:'31/07/2020'}
 ];
