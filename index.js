f55123f21d1f85412dab8a8967213d874e36f583
shani nemni <shanemni1@gmail.com>
2020-08-10 22:27:53 +0300

    index

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const express = require('express');
 const app = express();
 
 const BASE_URL = '/api'
-// //adding middleware
-// app.use(express.json());
 
 const productsRouter =  require('./src/server/routes/ProductsRoutes');
+const categoriesRouter = require('./src/server/routes/CategoriesRoutes');
+
 app.use(`${BASE_URL}/product`,productsRouter);
-
-
-// app.get('/',(req,res) => {
-//     if(!req.body.name) {
-//         res.status(400).send("names is requiered");
-//         return ; 
-//     }
-
-//     res.send("hello world!!");
-// });
-
+app.use(`${BASE_URL}/category`,categoriesRouter);
 
 //port for prduction env. we cant rely the port will be available
 const port = process.env.PORT || 3000;
